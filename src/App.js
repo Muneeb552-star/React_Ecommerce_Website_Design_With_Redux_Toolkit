c91e2656cfeba6b1d114c48ef14588f5c1bc25af
muneeb <64945537+Muneeb552-star@users.noreply.github.com>
2023-05-29 17:02:06 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App () {
           <Navbar />
           <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/category/:id' element={<Category />} />
+            // <Route path='/category/:id' element={<Category />} />
             <Route path='/cart' element={<Cart />} />
           </Routes>
           <Footer />
